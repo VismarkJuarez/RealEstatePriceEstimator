2f33dd815ac704d0d0ca1a11d526065e4e1aae37
VismarkJuarez <vismarkjuarez@gmail.com>
2020-02-09 18:46:54 -0600

    Added a css directory, and logic for returning the homepage when accessing the root directory.

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 
 //adding all required dependencies/packages
 const express = require('express');
+const path = require('path');
+const fs = require("fs");
 
 //specifies that this app will be using express.
 const app = express();
@@ -9,9 +11,16 @@ const app = express();
 //static AWS EC2 instance server port. Edit with caution.
 const serverPort = 5001;
 
+//Allow the use of static files in project directory
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
+app.use('/resources', express.static(__dirname + '/resources'));
+
+
 //Handle all root requests.
 app.get("/", function(req, res) {
-  res.send("Hello!");
+  res.sendFile(path.resolve("index.html"));
 });
 
 //Start-up behaviour.
