1f54d2cf9a4f34a07ace5c1599e396f7836f09ad
Vismark Juarez <vismarkjuarez@gmailcom>
2020-02-09 19:53:21 -0600

    Fixed nav bar so each tab navigates to the appropriate webpage.

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const serverPort = 5001;
 
 //Allow the use of static files in project directory
 app.use('/js', express.static(__dirname + '/js'));
+app.use('/html', express.static(__dirname + '/html'));
 app.use('/css', express.static(__dirname + '/css'));
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
 app.use('/resources', express.static(__dirname + '/resources'));
@@ -22,6 +23,28 @@ app.use('/resources', express.static(__dirname + '/resources'));
 app.get("/", function(req, res) {
   res.sendFile(path.resolve("index.html"));
 });
+
+app.get("/index", function(req, res) {
+  res.sendFile(path.resolve("index.html"));
+});
+
+app.get("/predictor", function(req, res) {
+  res.sendFile(path.resolve("html/predictor.html"));
+});
+
+app.get("/how_it_works", function(req, res) {
+  res.sendFile(path.resolve("html/how_it_works.html"));
+});
+
+app.get("/about_us", function(req, res) {
+  res.sendFile(path.resolve("html/about_us.html"));
+});
+
+
+
+
+
+
 
 //Start-up behaviour.
 app.listen(serverPort, function() {
