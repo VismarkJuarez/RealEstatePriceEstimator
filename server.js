c04028292e5fe4dcea1f5b8433e254f31bf5c392
VismarkJuarez <vismarkjuarez@gmail.com>
2020-03-10 18:12:28 -0500

    Logging the POST body to the console for a granular view of what information is flowing through the application and onto the fLASK app

--- server.js
+++ server.js
@@ -4,9 +4,13 @@
 const express = require('express');
 const path = require('path');
 const fs = require("fs");
+const bodyParser = require('body-parser'); //for parsing post requests
 
 //specifies that this app will be using express.
 const app = express();
+
+//middleware for processing POST requests a bit easier.
+app.use(bodyParser.urlencoded({extended: false}));
 
 //static AWS EC2 instance server port. Edit with caution.
 const serverPort = 5001;
@@ -17,6 +21,8 @@ app.use('/html', express.static(__dirname + '/html'));
 app.use('/css', express.static(__dirname + '/css'));
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
 app.use('/resources', express.static(__dirname + '/resources'));
+
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 
 //Handle all root requests.
@@ -52,7 +58,8 @@ app.get("/about_us", function(req, res) {
  * @param  response res The response to be returned from this function.
  * @return HTTP status     The status of the executed HTTP POST request.
  */
-app.post("/post_data_to_predictor_algorithm", function(req, res) {
+app.post("/post_data_to_predictor_algorithm", (req, res) => {
+  console.log("received the following payload for POSTing to the Flask server: \n" + req.body);
   res.send("THANK YOU");
 });
 
